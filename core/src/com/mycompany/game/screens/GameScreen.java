36b59a34937dd2bcf6b7e71534068d6ab33266ae
eeaann9044 <irhc74@student.cis.edu.hk>
2020-06-17 10:37:42 +0800

    adadad ad

--- core/src/com/mycompany/game/screens/GameScreen.java
+++ core/src/com/mycompany/game/screens/GameScreen.java
@@ -1,28 +1,51 @@
 package com.mycompany.game.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mycompany.game.Constants;
 import com.mycompany.game.MainClass;
 
+import javax.crypto.Cipher;
 
 
 public class GameScreen implements Screen {
     MainClass mainClass;
     Texture testTexture;
 
+    private Viewport viewport;
+    private OrthographicCamera camera;
+
     private TmxMapLoader mapLoader; //helps load the map
     private TiledMap map; //the loaded map object
     private OrthogonalTiledMapRenderer renderer; //renders the map
 
+    public void update(float delta)
+    {
+        camera.update();
+        renderer.setView(camera); //sets the view from our camera so it would render only what our camera can see.
+    }
+
     public GameScreen(MainClass game)
     {
+        mainClass = game;
         //existing constructor code
-        mapLoader = new TmxMapLoader(); //create an instance of built-in map loader object
-        map = mapLoader.load("tilemaps/lvl1.tmx"); //using map loader object, load the tiled map that you made
-        renderer = new OrthogonalTiledMapRenderer(map); //render the map.
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("tilemaps/lvl1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(Constants.WIDTH, Constants.HEIGHT, camera);
+        camera.position.set(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, 0);
+
+
     }
 
     @Override
@@ -32,12 +55,18 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
+
+        update(delta);
+        //clear screen
+        Gdx.gl.glClearColor(0, 0 , 0 ,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        mainClass.getBatch().setProjectionMatrix(camera.combined); //updates our batch with our Camera's view and projection matrices.
         renderer.render();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height);
     }
 
     @Override
